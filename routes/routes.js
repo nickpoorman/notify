c0a1b1b6a071529bd75e5e984d85c97b3de74496
Nick Poorman <nickpoorman@gmail.com>
2013-10-12 15:43:08 -0400

    added user auth

---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+/**
+ * routes.js
+ */
+
+module.exports = function(app) {
+  app.use(require('../modules/auth.js'));
+  app.use(require('../modules/session.js'));
+
+
+  app.get("/server/status", function(req, res) {
+    return res.type('txt').send('online');
+  });
+};
